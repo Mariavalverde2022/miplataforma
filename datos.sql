a062c79551df802bc9aad889fb4e7b72d9cfa5e2
Maria Jose <mariajosevaoverdebautista@gmail.comgit config --global user.email mariajosevalverdebautista@gmail.com>
2022-09-08 14:30:39 -0500

    base de datos

---
+++ datos.sql
@@ -0,0 +1,4 @@
+ create database miplataforma;
+ use miplataforma;
+
+ create table persona(idpersona int,nombres varchar(20),apellido varchar(20),cedula varchar(10),idgenero int,fechanacimiento date,idnacionaldiad int,idetnia int,idestadocivil int);
